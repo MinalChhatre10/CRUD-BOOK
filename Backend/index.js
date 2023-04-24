6aac677f3ea41ec7c723d1a9ea64bd93c699ff90
MinalChhatre10 <minalchhatre10@gmail.com>
2023-04-25 01:47:22 +0530

    Delete book

--- Backend/index.js
+++ Backend/index.js
@@ -49,7 +49,15 @@ app.post("/books", (req, res) => {
   });
 });
 
-//app.delete();
+app.delete("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = "DELETE FROM books WHERE id =?";
+
+  db.query(q, [bookId], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been deleted sucessfully");
+  });
+});
 
 app.listen(8800, () => {
   console.log("Connected to backend!");
