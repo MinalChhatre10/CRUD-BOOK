4d8fb3511796e29296f6944fd8c36ed0d509b678
MinalChhatre10 <minalchhatre10@gmail.com>
2023-04-25 20:46:15 +0530

    Style done

--- Client/client/src/Pages/Books.jsx
+++ Client/client/src/Pages/Books.jsx
@@ -30,7 +30,7 @@ const Books = () => {
 
   return (
     <div>
-      <h1>Kathaa</h1>
+      <h1>Books</h1>
       <div className="books">
         {books.map(book=>(
           <div className="book" key={book.id}>
